9505c06496ddbb6ce97cc5e4c04b43f25b9a51ff
tomasu <tomasu@3c4ecaba-1a45-11df-aecb-e3849e19168a>
2010-03-02 21:04:58 +0000

    - Cleaned up the code significantly. - Fixed a crashing bug caused by a texture memory leak. - Made the grid into separate boxes and tuned unit drawing functions.

--- trunk/Code/TestProject/glwidget.cpp
+++ trunk/Code/TestProject/glwidget.cpp
@@ -1,15 +1,30 @@
 #include "glwidget.h"
 
+///////////////////////////////////////////////////////////////////////////////
+//	Function Name:	GLWidget()
+//	Description:	Class constructor.
+//	Modified:	03/02/2010
+//	Author:		Tom Calloway
+///////////////////////////////////////////////////////////////////////////////
 GLWidget::GLWidget()
 {
-    cellCount = 10;
     startTimer( TIMER_INTERVAL );
-    rotX = rotY = rotZ = 0.f;
-    col = 0;
 }
 
-void GLWidget::initializeGL()
+///////////////////////////////////////////////////////////////////////////////
+//	Function Name:	debug_GenerateContent()
+//	Description:	Add content for unit testing.
+//	Modified:	03/02/2010
+//	Author:		Tom Calloway
+///////////////////////////////////////////////////////////////////////////////
+void GLWidget::debug_GenerateContent()
 {
+    // Select a battle map.
+    map.mapFilename = "backgrounds/grass.png";
+    map.cellsTall = 5;
+    map.cellsWide = 7;
+    map.gridHeight = 0.55;
+
     // Create a new unit (for gl debug purposes).
     unit[0].actionTime = 10;
     unit[0].actionRate = 150;
@@ -18,9 +33,9 @@ void GLWidget::initializeGL()
     unit[0].image.load("sprites/wizard.png");
     unit[0].mask_image.load("sprites/mask_wizard.png");
     unit[0].status = UNIT_OK;
-    unit[0].xLocation = 1;
-    unit[0].yLocation = 1;
-    unit[0].leftTeam = true;
+    unit[0].vLocation = 1;
+    unit[0].hLocation = 1;
+    unit[0].faceLeft = false;
 
     // Create a new unit (for gl debug purposes).
     unit[1].actionTime = 10;
@@ -30,9 +45,9 @@ void GLWidget::initializeGL()
     unit[1].image.load("sprites/buddhist.png");
     unit[1].mask_image.load("sprites/mask_buddhist.png");
     unit[1].status = UNIT_OK;
-    unit[1].xLocation = 3;
-    unit[1].yLocation = 4;
-    unit[1].leftTeam = true;
+    unit[1].vLocation = 0;
+    unit[1].hLocation = 1;
+    unit[1].faceLeft = false;
 
     // Create a new unit (for gl debug purposes).
     unit[2].actionTime = 40;
@@ -42,9 +57,9 @@ void GLWidget::initializeGL()
     unit[2].image.load("sprites/bard.png");
     unit[2].mask_image.load("sprites/mask_bard.png");
     unit[2].status = UNIT_OK;
-    unit[2].xLocation = 4;
-    unit[2].yLocation = 5;
-    unit[2].leftTeam = true;
+    unit[2].vLocation = 2;
+    unit[2].hLocation = 2;
+    unit[2].faceLeft = false;
 
     // Create a new unit (for gl debug purposes).
     unit[3].actionTime = 40;
@@ -54,9 +69,9 @@ void GLWidget::initializeGL()
     unit[3].image.load("sprites/desertsoldier.png");
     unit[3].mask_image.load("sprites/mask_desertsoldier.png");
     unit[3].status = UNIT_OK;
-    unit[3].xLocation = 2;
-    unit[3].yLocation = 9;
-    unit[3].leftTeam = false;
+    unit[3].vLocation = 3;
+    unit[3].hLocation = 5;
+    unit[3].faceLeft = true;
 
     // Create a new unit (for gl debug purposes).
     unit[4].actionTime = 40;
@@ -66,9 +81,9 @@ void GLWidget::initializeGL()
     unit[4].image.load("sprites/merchant.png");
     unit[4].mask_image.load("sprites/mask_merchant.png");
     unit[4].status = UNIT_OK;
-    unit[4].xLocation = 3;
-    unit[4].yLocation = 7;
-    unit[4].leftTeam = false;
+    unit[4].vLocation = 2;
+    unit[4].hLocation = 5;
+    unit[4].faceLeft = true;
 
     // Create a new unit (for gl debug purposes).
     unit[5].actionTime = 40;
@@ -78,18 +93,29 @@ void GLWidget::initializeGL()
     unit[5].image.load("sprites/priestess.png");
     unit[5].mask_image.load("sprites/mask_priestess.png");
     unit[5].status = UNIT_OK;
-    unit[5].xLocation = 2;
-    unit[5].yLocation = 6;
-    unit[5].leftTeam = false;
+    unit[5].vLocation = 1;
+    unit[5].hLocation = 6;
+    unit[5].faceLeft = true;
 
-    for( int i = 6; i < MAX_UNITS; i++ )
+    for (int i = 6; i < MAX_UNITS; i++)
     {
         // Tell OpenGL whether or not the unit exists.
         unit[i].status = NO_UNIT;
     }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//	Function Name:	initializeGL()
+//	Description:	Initialize the OpenGL widget.
+//	Modified:	03/02/2010
+//	Author:		Tom Calloway
+///////////////////////////////////////////////////////////////////////////////
+void GLWidget::initializeGL()
+{
+    debug_GenerateContent();
 
     // Load a background image (debug).
-    bkImage.load("backgrounds/beach.png");
+    bkImage.load(map.mapFilename.c_str());
     bkImage = bkImage.scaled(GLWidget::width(), GLWidget::height(), Qt::KeepAspectRatio, Qt::SmoothTransformation );
     glBkImage = QGLWidget::convertToGLFormat(bkImage);
 
@@ -99,66 +125,133 @@ void GLWidget::initializeGL()
     glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);				// Set Line Antialiasing
     glEnable(GL_BLEND);                                                 // Enable Blending
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);			// Type Of Blending To Use
-    glEnable(GL_TEXTURE_2D);
-    return;								// Initialization Went OK
+
+    return;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+//	Function Name:	paintGL()
+//	Description:	Paint the OpenGL widget (run on a timer).
+//	Modified:	03/02/2010
+//	Author:		Tom Calloway
+///////////////////////////////////////////////////////////////////////////////
 void GLWidget::paintGL()
 {
-    // (Re)Calculate the OpenGL object dimensions.
-    fullWidth = GLWidget::width();
-    fullHeight = GLWidget::height();
-    statusWidth = (fullWidth / cellCount) * 0.1f;
-    cellWidth = fullWidth / cellCount;
-    cellHeight = fullHeight / cellCount;
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
     // Draw the background.
     glDrawPixels(glBkImage.width(), glBkImage.height(), GL_RGBA, GL_UNSIGNED_BYTE, glBkImage.bits());
-
-    // Draw all units.
-    for( int i = 0; i < MAX_UNITS; i++ )
+    
+    // Draw the grid.
+    for (int i = 0; i < map.cellsWide; i++)
     {
-        drawUnit(unit[i]);
-        if( unit[i].actionTime >= 100 )
+        for (int j = 0; j < map.cellsTall; j++)
+        {
+            if (i == 1 && j == 1)
+            {
+                drawGridBox(i, j, true);
+            }
+            else
+            {
+                drawGridBox(i, j, false);
+            }
+        }
+    }
+
+    // Draw the units.
+    for (int i = 0; i < MAX_UNITS; i++)
+    {
+        updateUnit(unit[i]);
+        if (unit[i].actionTime >= 100)
         {
             //killTimer(1);
             unit[i].actionTime = 0;
         }
         unit[i].actionTime += 0.005 * unit[i].actionRate;
     }
-    
-    // Make the grid lines white.
+
+    // Draw the header graphics.
     qglColor(Qt::white);
-
-    // Draw the grid lines.
-    glBegin(GL_LINES);
-    for (int i = 1; i <= cellCount + 2; i++)
-    {
-        // Vertical lines.
-        glVertex3f(fullWidth/cellCount * i, 0.0f, 0.0f); // Origin of the line.
-        glVertex3f(fullWidth/cellCount * i , fullHeight, 0.0f); // Ending point of the line.
-    }
-    for (int i = 1; i <= 12; i++)
-    {
-        // Horizontal lines.
-        glVertex3f(0.0f, fullHeight/cellCount * i, 0.0f); // Origin of the line.
-        glVertex3f(fullWidth, fullHeight/cellCount * i, 0.0f); // Ending point of the line.
-    }
-    glEnd( );
-
-    // Write some text.
-    //qglColor(Qt::white);
-    //renderText(330, 310, 0.0, "Tom text!");
+    renderText(20, fullHeight + 150, 0.0, "Header graphics coming soon...");
 }
 
-bool GLWidget::drawUnit(characterUnit myUnit)
+///////////////////////////////////////////////////////////////////////////////
+//	Function Name:	drawGridBox()
+//	Description:	Draw the requested grid box.
+//                      Call this fuction in a loop to get full grid.
+//                      If 'isSelected' is true, the box is red.
+//	Modified:	03/02/2010
+//	Author:		Tom Calloway
+///////////////////////////////////////////////////////////////////////////////
+bool GLWidget::drawGridBox(int cellFromLeft, int cellFromBottom, bool isSelected)
 {
-    if( myUnit.status == NO_UNIT )
+    // Define full (average) cell dimensions.
+    float cellWidth = fullWidth / map.cellsWide;
+    float cellHeight = fullHeight / map.cellsTall;
+    float leftEdge = cellFromLeft * cellWidth;
+    float bottomEdge = cellFromBottom * cellHeight;// - (cellHeight * 1.2);
+
+    // Cell border style.
+    int padding = 3;
+    if (isSelected)
     {
-        return( false );
+        int lineWidth = 2;
+        glLineWidth(lineWidth);
+        glColor4f( 0.4f, 0.0f, 0.0f, 0.8f );
+    }
+    else
+    {
+        int lineWidth = 1;
+        glLineWidth(lineWidth);
+        glColor4f( 0.4f, 0.4f, 0.4f, 0.8f );
+    }
+
+    // Define corner locations (without perspective).
+    point ulLoc, urLoc, blLoc, brLoc;
+    blLoc.hLoc = leftEdge + padding;
+    blLoc.vLoc = bottomEdge + padding;
+    brLoc.hLoc = leftEdge + cellWidth - padding;
+    brLoc.vLoc = bottomEdge + padding;
+    ulLoc.hLoc = leftEdge + padding;
+    ulLoc.vLoc = bottomEdge + cellHeight - padding;
+    urLoc.hLoc = leftEdge + cellWidth - padding;
+    urLoc.vLoc = bottomEdge + cellHeight - padding;
+
+    glRotatef(-5, 1.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+        // Left line.
+        glVertex3f(blLoc.hLoc, blLoc.vLoc, 0.0f);
+        glVertex3f(ulLoc.hLoc, ulLoc.vLoc, 0.0f);
+
+        // Top line.
+        glVertex3f(ulLoc.hLoc, ulLoc.vLoc, 0.0f);
+        glVertex3f(urLoc.hLoc, urLoc.vLoc, 0.0f);
+
+        // Right line.
+        glVertex3f(brLoc.hLoc, brLoc.vLoc, 0.0f);
+        glVertex3f(urLoc.hLoc, urLoc.vLoc, 0.0f);
+
+        // Bottom line.
+        glVertex3f(blLoc.hLoc, blLoc.vLoc, 0.0f);
+        glVertex3f(brLoc.hLoc, brLoc.vLoc, 0.0f);
+    glEnd();
+    glRotatef(5, 1.0, 0.0, 0.0);
+
+    return (true);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//	Function Name:	updateUnit()
+//	Description:	Update the requested Unit in the OpenGL widget.
+//	Modified:	03/02/2010
+//	Author:		Tom Calloway
+///////////////////////////////////////////////////////////////////////////////
+bool GLWidget::updateUnit(characterUnit myUnit)
+{
+    if (myUnit.status == NO_UNIT)
+    {
+        return (false);
     }
 
     // Calculate the action and hit point bars as a percentage.
@@ -166,23 +259,23 @@ bool GLWidget::drawUnit(characterUnit myUnit)
     float hitPoints = (float)myUnit.hitPoints / (float)myUnit.totalHitPoints;
 
     // Dynamically define the height and width of the sprite.
-    float widthRation = (float)myUnit.image.width() / (float)myUnit.image.height();
-    int spriteHeight = cellHeight + 1, spriteWidth = cellHeight * widthRation;
+    float widthRatio = (float)myUnit.image.width() / (float)myUnit.image.height();
+    int spriteHeight = cellHeight * 1.5 + 1;
+    int spriteWidth = cellHeight * 1.5 * widthRatio;
 
     // Dynamically define the OpenGL location of the sprite.
-    int bottomEdge = myUnit.xLocation * cellHeight;
-    int leftEdge = myUnit.yLocation * cellWidth + (cellWidth / 2) - (spriteWidth / 2);
+    int bottomEdge = myUnit.vLocation * cellHeight + spriteHeight/6;
+    int leftEdge = myUnit.hLocation * cellWidth + (cellWidth / 2) - (spriteWidth / 2);
     int rightEdge = leftEdge + spriteWidth;
 
-    glColor4f(1.0f,1.0f,1.0f,0.5f);
-    glBlendFunc(GL_DST_COLOR,GL_ZERO);
+    glColor4f( 1.0f, 1.0f, 1.0f, 0.5f );
+    glBlendFunc( GL_DST_COLOR,GL_ZERO );
 
     glEnable(GL_TEXTURE_2D);
     GLuint textureMask = bindTexture( myUnit.mask_image, GL_TEXTURE_2D );
-    glBindTexture( GL_TEXTURE_2D, textureMask );
 
     // Draw the sprite facing left or right.
-    if( !myUnit.leftTeam )
+    if (myUnit.faceLeft)
     {
         glBegin (GL_QUADS);
             // Bottom left.
@@ -200,7 +293,7 @@ bool GLWidget::drawUnit(characterUnit myUnit)
             // Top left.
             glTexCoord2f (0.0, 1.0);
             glVertex3f (leftEdge, bottomEdge + spriteHeight, 0.0);
-        glEnd ();
+        glEnd();
     }
     else
     {
@@ -225,10 +318,9 @@ bool GLWidget::drawUnit(characterUnit myUnit)
 
     glBlendFunc(GL_ONE, GL_ONE);
     GLuint textureValue = bindTexture( myUnit.image, GL_TEXTURE_2D );
-    glBindTexture( GL_TEXTURE_2D, textureValue );
 
     // Draw the sprite facing left or right.
-    if( !myUnit.leftTeam )
+    if (myUnit.faceLeft)
     {
         glBegin (GL_QUADS);
             // Bottom left.
@@ -250,7 +342,7 @@ bool GLWidget::drawUnit(characterUnit myUnit)
     }
     else
     {
-        glBegin (GL_QUADS);
+        glBegin(GL_QUADS);
             // Bottom left.
             glTexCoord2f (1.0, 0.0);
             glVertex3f (leftEdge, bottomEdge, 0.0);
@@ -266,66 +358,112 @@ bool GLWidget::drawUnit(characterUnit myUnit)
             // Top left.
             glTexCoord2f (1.0, 1.0);
             glVertex3f (leftEdge, bottomEdge + spriteHeight, 0.0);
-        glEnd ();
+        glEnd();
     }
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glDisable(GL_TEXTURE_2D);
 
+    // Free memory.
+    glDeleteTextures( 1, &textureMask );
+    glDeleteTextures( 1, &textureValue );
+
     // Show unit's health.
+    glColor3f(0.3f, 0.3f, 0.3f);
+    glBegin(GL_QUADS);
+        glVertex3f( leftEdge - statusWidth, bottomEdge + cellHeight/2, 0.0f );
+        glVertex3f( leftEdge, bottomEdge + cellHeight/2, 0.0f );
+        glVertex3f( leftEdge,  bottomEdge, 0.0f );
+        glVertex3f( leftEdge - statusWidth, bottomEdge, 0.0f );
+    glEnd();
     glColor3f(0.6f, 0.0f, 0.0f);
     glBegin(GL_QUADS);
-        glVertex3f( leftEdge - statusWidth, bottomEdge + cellHeight * hitPoints, 0.0f );	// Top Left
-        glVertex3f( leftEdge, bottomEdge + cellHeight * hitPoints, 0.0f );	// Top Right
-        glVertex3f( leftEdge,  bottomEdge, 0.0f );	// Bottom Right
-        glVertex3f( leftEdge - statusWidth, bottomEdge, 0.0f );	// Bottom Left
+        glVertex3f( leftEdge - statusWidth, bottomEdge + cellHeight/2 * hitPoints, 0.0f );
+        glVertex3f( leftEdge, bottomEdge + cellHeight/2 * hitPoints, 0.0f );
+        glVertex3f( leftEdge,  bottomEdge, 0.0f );
+        glVertex3f( leftEdge - statusWidth, bottomEdge, 0.0f );
     glEnd();
 
     // Show unit's action points.
-    glColor3f(0.0f, 0.0f, actionTime);
+    glColor3f(0.3f, 0.3f, 0.3f);
     glBegin(GL_QUADS);
-        glVertex3f( rightEdge, bottomEdge + cellHeight * actionTime, 0.0f );	// Top Left
-        glVertex3f( rightEdge + statusWidth, bottomEdge + cellHeight * actionTime, 0.0f );	//Top Right
+        glVertex3f( rightEdge, bottomEdge + cellHeight/2, 0.0f );	// Top Left
+        glVertex3f( rightEdge + statusWidth, bottomEdge + cellHeight/2, 0.0f );	//Top Right
         glVertex3f( rightEdge + statusWidth, bottomEdge, 0.0f );	// Bottom Right
         glVertex3f( rightEdge, bottomEdge, 0.0f );	// Bottom Left
     glEnd();
-    return( true );
+    glColor3f(0.0f, 0.0f, 0.6f);
+    glBegin(GL_QUADS);
+        glVertex3f( rightEdge, bottomEdge + cellHeight/2 * actionTime, 0.0f );	// Top Left
+        glVertex3f( rightEdge + statusWidth, bottomEdge + cellHeight/2 * actionTime, 0.0f );	//Top Right
+        glVertex3f( rightEdge + statusWidth, bottomEdge, 0.0f );	// Bottom Right
+        glVertex3f( rightEdge, bottomEdge, 0.0f );	// Bottom Left
+    glEnd();
+
+    return (true);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+//	Function Name:	resizeGL()
+//	Description:	Adjust drawing element dimensions on window resize.
+//	Modified:	03/02/2010
+//	Author:		Tom Calloway
+///////////////////////////////////////////////////////////////////////////////
 void GLWidget::resizeGL(int width, int height)
 {
     // Never divide by zero.
-    if (height==0)
+    if (height == 0)
     {
         height=1;
     }
 
-    // Setup the new viewport.
-    glViewport (0, 0, width, height);
+    // (Re)Calculate the OpenGL object dimensions.
+    fullWidth = width;
+    fullHeight = height * map.gridHeight;
+    statusWidth = (fullWidth / map.cellsWide) * 0.1f;
+    cellWidth = fullWidth / map.cellsWide;
+    cellHeight = fullHeight / map.cellsTall;
+
+    // Setup the 2d perspective.
+    glViewport(0, 0, width, height);
     glMatrixMode (GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0, width,0,height,-1,1);
+    glOrtho(0, width, 0, height, -1000, 1000);
+
+    // Use a 3d perspective.
+    //float aspectRatio = width / height;
+    //float viewPortSize = width * height;
+    //gluPerspective( 90, aspectRation, 1, 10000 );
+    //gluLookAt( width/2, height/2, viewPortSize/850, width/2, height/2, 0, 0.0, 1.0, 0.0);
+
     glMatrixMode (GL_MODELVIEW);
+    glLoadIdentity();
 
     updateGL();
 
     // Reload and resize the background image.
-    bkImage.load("backgrounds/beach.png");
-    bkImage = bkImage.scaled(fullWidth, fullHeight, Qt::IgnoreAspectRatio, Qt::SmoothTransformation );
+    bkImage.load(map.mapFilename.c_str());
+    bkImage = bkImage.scaled(GLWidget::width(), GLWidget::height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation );
     glBkImage = QGLWidget::convertToGLFormat(bkImage);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//	Function Name:	timerEvent()
+//	Description:	Redraw the OpenGl widget whenever the timer triggers.
+//	Modified:	03/02/2010
+//	Author:		Tom Calloway
+///////////////////////////////////////////////////////////////////////////////
+void GLWidget::timerEvent(QTimerEvent *event)
+{
+    updateGL();
 }
 
 void GLWidget::mousePressEvent(QMouseEvent *event)
 {
-  //proces mouse events for rotate/move inside 3D scene
+    // Proces mouse events for rotate/move inside 3D scene
 }
 
 void GLWidget::mouseMoveEvent(QMouseEvent *event)
 {
     // Process mouse stuff...
-}
-
-void GLWidget::timerEvent(QTimerEvent *event)
-{
-    updateGL();
 }
