47559d3f11192638adb2e28427622cc3e7f016a8
obi.atueyi <obi.atueyi@3c4ecaba-1a45-11df-aecb-e3849e19168a>
2010-04-26 06:58:18 +0000

    Modified code to include test database header

---
+++ Code/newgamewizard/test_db.h
@@ -0,0 +1,18 @@
+#ifndef TEST_DB_H
+#define TEST_DB_H
+
+//#include <string>
+
+class QImage;
+
+class Sprite
+{
+public:
+    QString name;
+    QImage  image;
+    int     AP;
+    int     HP;
+    int     range;
+};
+
+#endif // TEST_DB_H
