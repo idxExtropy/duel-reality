6270d1dbb86b3e31d723678c5c3c8895188d75e1
obi.atueyi <obi.atueyi@3c4ecaba-1a45-11df-aecb-e3849e19168a>
2010-04-29 05:47:53 +0000

    Modified headers. Created test code for database functions

--- trunk/Code/newgamewizard/globals.h
+++ trunk/Code/newgamewizard/globals.h
@@ -2,49 +2,66 @@
 #define GLOBALS_H
 
 
+/*---------------- I n c l u d e s ----------------*/
+
 #include <string>
 using std::string;
 
-static const int    MAX_SPRITES = 4;
-static const int    MAX_UNITS = 4;
 
+/*---------------- C o n s t a n t    d e f i n i t i o n s ----------------*/
+
+static const int    MAX_SPRITES = 4;
+static const int    MAX_UNITS = 4;      // Maximum number of units per player
+static const int    NO_UNIT = 0;        // Unit status (unit doesn't exist)
+static const int    UNIT_DEAD = 1;      // Unit status (unit is dead)
+static const int    UNIT_OK = 2;        // Unit status (unit is alive)
+static const int    USER_UNIT = 1;      // Indicates unit is associated with user
+static const int    AI_UNIT = 2;        // Indicates unit is associated with AI
+
+
+/*---------------- C l a s s    d e c l a r a t i o n s ----------------*/
 
 class QImage;
 
 
+/*---------------- C l a s s    d e f i n i t i o n s ----------------*/
+
 class characterUnit
 {
 public:
-    bool    isAlive;
-    string  name;
-    bool    faceLeft;
-    int     vLocation;
-    int     hLocation;
-    int     hitPoints;
-    int     totalHitPoints;
+    string  name;               // Unit name
+    bool    faceLeft;           // Unit orientation
+    int     vLocation;          // Unit vertical location
+    int     hLocation;          // Unit horizontal location
+    int     hitPoints;          // Unit hit points
+    int     totalHitPoints;     // Total unit hit points
     float   actionTime;
     int     actionRate;
     int     movementRate;
     int     attackRange;
     int     attackPower;
-    int     status;
-    QImage  image;
-    QImage  mask_image;
+    int     status;             // Unit status
+    QImage  image;              // Associated unit image
+    QImage  mask_image;         // Associate unit mask image
+    int     team;               // Unit's associated team
+    int     actionPoints;
+    int     totalActionPoints;
+    int     unitWorth;          // Amount of XP awarded for killing unit
 };
 
 
 class Player
 {
 public:
-    characterUnit   units[MAX_UNITS];
+    characterUnit   units[MAX_UNITS];   // Player units
 };
 
 
 class User : public Player
 {
 public:
-    string  playerName;
-    int     experiencePoints;
+    string  playerName;         // Player name
+    int     experiencePoints;   // Player experience points
 };
 
 
--- trunk/Code/newgamewizard/test_db.h
+++ trunk/Code/newgamewizard/test_db.h
@@ -2,15 +2,36 @@
 #define TEST_DB_H
 
 
+/*---------------- C l a s s    d e c l a r a t i o n s ----------------*/
+
+class User
+
+
+/*---------------- C l a s s    d e f i n i t i o n s ----------------*/
+
 class Sprite
 {
 public:
-    QString name;
-    QPixmap pixMap;
+    //QString name;
+    string  name;
+    string  spriteFileName;
+    //QPixmap pixMap;
     int     AP;
     int     HP;
     int     range;
 };
+
+
+/*---------------- F u n c t i o n    P r o t o t y p e s ----------------*/
+
+int GetSpriteCount(void);           // Returns total number of sprite data entries
+string GetSpriteName(int index);    // Returns sprite name in entry index
+Sprite GetSpriteData(string name);  // Returns sprite data for associated sprite name
+
+User CreateUserData(string name);   // Creates user player data
+int GetUserCount(void);             // Returns total number of user player data entries
+string GetUserName(int index);      // Returns user player name in entry index
+User GetUserData(string name);      // Returns user player data for associate user player name
 
 
 #endif // TEST_DB_H
