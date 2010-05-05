ea307325c355f6ed67cf884ca5d715ce90612e1a
kilgore780 <kilgore780@3c4ecaba-1a45-11df-aecb-e3849e19168a>
2010-05-05 23:32:29 +0000

    cleaned out my files that weren't in use, changes mechanics class.

--- trunk/Code/duelreality/mechanics.h
+++ trunk/Code/duelreality/mechanics.h
@@ -1,34 +1,35 @@
 #ifndef MECHANICS_H
 #define MECHANICS_H
+#include "globals.h"
+#include <QObject>
 
-#include <string>
-#include <stdio.h>
-#include <fstream>
-#include <iostream>
-#include "unit.h"
-#include "jmap.h"
-#include "glwidget.h"
-//#include "player.h"
+class Unit;
 
-class mechanics
+class mechanics : public QObject
 {
+Q_OBJECT
 public:
-        mechanics();
-        ~mechanics(void);
-        void moveUP(unit a);
-        void moveDOWN(unit a);
-        void moveLEFT(unit a);
-        void moveRIGHT(unit a);
-        void Move(unit unittomove);
-      //  void initiate();
-        bool isValidMove(unit a, int targetx, int targety);
-        bool isOccupied(int b, int c);
-        void attack(unit a, unit target);
-        bool isValidAttack(unit a, unit target);
-        //bool isGameOver()
-        //unit upgradeUnit(unit)
-        bool isSufficientAP (int AP, int moveRate);
-};
+    explicit mechanics(QObject *parent = 0);
 
+signals:
+
+public slots:
+ //void move();
+ //void attack();
+ //void endTurn();
+
+private:
+    void move(Unit a, int targetx, int targety);
+    bool isValidMove(Unit a, int targetx, int targety);
+    bool isOccupied(int x, int y);
+    bool isSufficientAP(Unit a);
+    Unit getUnit(int x, int y);
+    Unit setFocus(int x, int y);
+    //void startTurn(Unit team[4]);
+    void endTurn();
+
+    Unit *UnitPointer;
+
+};
 
 #endif // MECHANICS_H
