7a6b519091aa166c6b000a759a1db646010c6022
kilgore780 <kilgore780@3c4ecaba-1a45-11df-aecb-e3849e19168a>
2010-05-06 02:06:42 +0000

    more mechanics changes. need to connect sigs/slots

--- trunk/Code/duelreality/mechanics.h
+++ trunk/Code/duelreality/mechanics.h
@@ -2,33 +2,54 @@
 #define MECHANICS_H
 #include "globals.h"
 #include <QObject>
+#include <QWidget>
+#include <QTimer>
+#include <QString>
 
 class Unit;
+class test_db;
 
 class mechanics : public QObject
 {
 Q_OBJECT
 public:
     explicit mechanics(QObject *parent = 0);
+    ~mechanics();
+    bool isRunning() {return gamerunning;}
+      void setRunnint(bool run) {gamerunning=run;}
 
 signals:
+// signalP1wins();
+// signalP2wins();
+// signalSwitchPlayer();
+// signalMoveDone();
+// signalAttackDone();
+// signalError();
+// startAI();
+
 
 public slots:
- //void move();
- //void attack();
- //void endTurn();
+ //void move(unit *a, int x, int y);
+ //void attack(unit *a, int x int y);
+ //void endTurn(int player);
+
 
 private:
     void move(Unit a, int targetx, int targety);
     bool isValidMove(Unit a, int targetx, int targety);
     bool isOccupied(int x, int y);
     bool isSufficientAP(Unit a);
+
+    int nextPlayer() {return iNextplayer;}
+    void setNextPlayer(int player){iNextplayer=player;}
     Unit getUnit(int x, int y);
     Unit setFocus(int x, int y);
     //void startTurn(Unit team[4]);
     void endTurn();
-
+    int isGameOver(Unit a[4]);
     Unit *UnitPointer;
+    bool gamerunning;
+    int iNextplayer;
 
 };
 
