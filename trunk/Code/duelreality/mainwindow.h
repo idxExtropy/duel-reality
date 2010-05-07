a59003a2100f5c247b8f0499378f74b024c06776
obi.atueyi <obi.atueyi@3c4ecaba-1a45-11df-aecb-e3849e19168a>
2010-05-07 23:12:15 +0000

    Added public signals and slots. Added test signals.

--- trunk/Code/duelreality/mainwindow.h
+++ trunk/Code/duelreality/mainwindow.h
@@ -25,27 +25,31 @@ protected:
 signals:
     void signalGameCfgComplete();   // Indicates new game or load game dialog is complete
     void signalUserTurnEnd();       // Indicates that user has ended turn
+    void isBattleMode(bool);        // True if game is in battle mode; false otherwise
+    void isGameCfgMode(bool);       // True if game is in config mode; false otherwise
+    void isUserTurn(bool);          // True is user's turn; false otherwise
+
+public slots:
+    void onBattleStart();   // Disables game config user interactions
+    void onBattleEnd();     // Disables battle interactions
+    void onUserTurn();      // Enables user turn interactions
 
 private slots:
     void newGame();     // Calls New Game dialogs
     void about();       // Calls About Game dialogs
     //void loadGame();    // Calls Load Game dialogs
     //bool exitGame();    // Exits game
-    //bool saveGame();    // Calls Save Game dialog
-    //void setActiveBattleFlag();
-    //void resetActiveBattleFlag();
-    //void setPlayerTurnFlag();
-    //void resetPlayerTurnFlag();
-
-    void onBattleStart();   // Disables game config user interactions
-    void onBattleEnd();     // Disables battle interactions
-    void onUserTurn();      // Enables user turn interactions
+    //bool saveGame();    // Calls Save Game dialog    
+    void endTurnClicked();  // Disables user turn interactions
 
 private:
     void createActions();   // Creates actions
     void createMenus();     // Creates menus
     void createToolBars();  // Creates toolbars
     void createStatusBar(); // Creates status bars
+
+    // Test methods
+    void createTestTools();
 
     // Modules from other team members
     QWidget     *centralWidget;
@@ -75,6 +79,12 @@ private:
     // Toolbar items
     QToolBar    *gameToolBar;   // Game toolbar
     QToolBar    *actionToolBar; // Action toolbar
+
+    // Test Members
+    QToolBar    *testToolBar;
+    QAction     *actionTestSignalBattleStart;
+    QAction     *actionTestSignalUserTurnStart;
+    QAction     *actionTestSignalBattleEnd;
 };
 
 #endif // MAINWINDOW_H
